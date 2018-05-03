ea9884ede3f9747597a73512122d9c9830e62192
Andrew Donley <andrewjdonley@gmail.com>
2018-05-03 05:49:52 -0700

    tx api workin

--- src/main/java/io/block16/assetapi/TransactionFunction.java
+++ src/main/java/io/block16/assetapi/TransactionFunction.java
@@ -5,7 +5,7 @@ import io.block16.assetapi.service.AddressTransactionService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
