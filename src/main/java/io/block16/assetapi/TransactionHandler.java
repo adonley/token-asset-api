50ba352d2ecfec092f23c08b363ba17c10b27d37
Andrew Donley <andrewjdonley@gmail.com>
2018-05-21 16:15:33 -0700

    added necessary lib

--- src/main/java/io/block16/assetapi/TransactionHandler.java
+++ src/main/java/io/block16/assetapi/TransactionHandler.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 
