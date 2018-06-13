7edc0586e473558d408fac0bda71889d0e15a212
Andrew Donley <andrewjdonley@gmail.com>
2018-06-13 00:42:29 -0700

    ratelimit working

--- src/main/java/io/block16/assetapi/ratelimit/RateLimiterInterceptor.java
+++ src/main/java/io/block16/assetapi/ratelimit/RateLimiterInterceptor.java
@@ -4,44 +4,93 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.annotation.PreDestroy;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.time.Instant;
+import java.util.Calendar;
+import java.util.concurrent.TimeUnit;
+
 
 public class RateLimiterInterceptor extends HandlerInterceptorAdapter {
     private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 
-    @Value("${rate.limit.enabled:true}")
-    private boolean enabled;
-    @Value("${rate.limit.minute:20}")
-    private int minuteLimit;
+    private int minuteLimit = 30;
+    private final long oneMinute = 60 * 1000;
+    private final ValueOperations<String, Object> valueOperations;
 
-    @Autowired
-    public RateLimiterInterceptor() {
+    public RateLimiterInterceptor(RedisTemplate<String, Object> redisTemplate) {
+        this.valueOperations = redisTemplate.opsForValue();
+    }
 
+    private static final String[] IP_HEADER_CANDIDATES = {
+            "X-Forwarded-For",
+            "Proxy-Client-IP",
+            "WL-Proxy-Client-IP",
+            "HTTP_X_FORWARDED_FOR",
+            "HTTP_X_FORWARDED",
+            "HTTP_X_CLUSTER_CLIENT_IP",
+            "HTTP_CLIENT_IP",
+            "HTTP_FORWARDED_FOR",
+            "HTTP_FORWARDED",
+            "HTTP_VIA",
+            "REMOTE_ADDR"
+    };
+
+    private String getClientIpAddress(HttpServletRequest request) {
+        for (String header : IP_HEADER_CANDIDATES) {
+            String ip = request.getHeader(header);
+            if (ip != null && ip.length() != 0 && !"unknown".equalsIgnoreCase(ip)) {
+                return ip;
+            }
+        }
+        return request.getRemoteAddr();
     }
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        boolean allowRequest = true;
 
-        if (!enabled) {
+        // Get the IP address from the incoming headers
+        String ipAddress = this.getClientIpAddress(request);
+        Object cachedInfoObj = this.valueOperations.get(ipAddress);
+        RateLimitDTO rateLimitDTO;
+
+        // If we don't have related object, create a new one
+        if (cachedInfoObj == null) {
+            rateLimitDTO = new RateLimitDTO();
+            rateLimitDTO.setBanned(false);
+            rateLimitDTO.setRequestCount(1);
+            rateLimitDTO.setTimestamp(Instant.now().toEpochMilli());
+            // Expire after a minute
+            this.valueOperations.set(ipAddress, rateLimitDTO, 1, TimeUnit.MINUTES);
             return true;
         }
 
-        if (!allowRequest) {
-            response.setStatus(HttpStatus.TOO_MANY_REQUESTS.value());
-        }
+        rateLimitDTO = (RateLimitDTO) cachedInfoObj;
+
+        // Request count
+        rateLimitDTO.setRequestCount(rateLimitDTO.getRequestCount() + 1);
+        long timeRemaining = rateLimitDTO.getTimestamp() + oneMinute - Instant.now().toEpochMilli();
+        this.valueOperations.set(ipAddress, rateLimitDTO, Math.max(timeRemaining, 0), TimeUnit.MILLISECONDS);
+        int remainingRequests = Math.max(minuteLimit - rateLimitDTO.getRequestCount(), 0);
+        response.addHeader("X-RateLimit-Remaining", String.valueOf(remainingRequests));
         response.addHeader("X-RateLimit-Limit", String.valueOf(minuteLimit));
 
-        return allowRequest;
-    }
+        LOGGER.debug("Remaining time: {}, Request remaining: {}", timeRemaining, remainingRequests);
 
-    @PreDestroy
-    public void destroy() {
-        // loop and finalize all limiters
+        // Should allow
+        boolean allowRequest = rateLimitDTO.getRequestCount() <= minuteLimit;
+
+        if(!allowRequest) {
+            response.setStatus(HttpStatus.TOO_MANY_REQUESTS.value());
+            return false;
+        }
+        return true;
     }
 }
